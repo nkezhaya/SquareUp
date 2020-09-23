f477ca80ce811906d515bdc58241291eb2eb56b2
Derek Kraan <derek.kraan@gmail.com>
2020-09-23 08:41:03 +0200

    `mix new square_up`

---
+++ lib/square_up.ex
@@ -0,0 +1,18 @@
+defmodule SquareUp do
+  @moduledoc """
+  Documentation for `SquareUp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SquareUp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/square_up_test.exs
@@ -0,0 +1,8 @@
+defmodule SquareUpTest do
+  use ExUnit.Case
+  doctest SquareUp
+
+  test "greets the world" do
+    assert SquareUp.hello() == :world
+  end
+end
