b9003becc46a6e818ac08d5b297c0780a6253e9d
Derek Kraan <derek.kraan@gmail.com>
2020-09-23 18:03:41 +0200

    Handle non-200 responses

--- lib/square_up/client.ex
+++ lib/square_up/client.ex
@@ -13,8 +13,24 @@ defmodule SquareUp.Client do
     )
     |> case do
       {:ok, 200, _resp_headers, ref} ->
-        {:ok, body_json} = :hackney.body(ref)
-        {:ok, Jason.decode!(body_json)}
+        {:ok, get_body!(ref)}
+
+      {:ok, http_code, _resp_headers, ref} ->
+        {:error, {:resp_not_200, http_code, get_body_best_effort(ref)}}
+    end
+  end
+
+  defp get_body!(ref) do
+    {:ok, body_json} = :hackney.body(ref)
+    Jason.decode!(body_json)
+  end
+
+  defp get_body_best_effort(ref) do
+    {:ok, body_json} = :hackney.body(ref)
+
+    case Jason.decode(body_json) do
+      {:ok, body} -> body
+      _ -> body_json
     end
   end
 
