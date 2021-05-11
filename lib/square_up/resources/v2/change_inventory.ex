e45256c0103d52bf743fefa5fed3f9135c089bc9
Pier-Olivier <pier-olivier@rumandcode.io>
2021-05-11 10:42:03 -0400

    disable norm, fail when passing array

--- lib/square_up/resources/v2/change_inventory.ex
+++ lib/square_up/resources/v2/change_inventory.ex
@@ -6,7 +6,9 @@ defmodule SquareUp.V2.ChangeInventory do
           SquareUp.Client.response(SquareUp.TypeSpecs.batch_change_inventory_response())
   def batch(client, path_params \\ %{}, params \\ %{}, query_params \\ %{}) do
     path_params_spec = schema(%{})
-    params_spec = Norm.Delegate.delegate(&SquareUp.NormSchema.batch_change_inventory_request/0)
+    # params_spec = Norm.Delegate.delegate(&SquareUp.NormSchema.upsert_catalog_object_request/0)
+    # Disable Norm check of params because passing an array of binaries is crashing the check.
+    params_spec = schema(%{})
     query_params_spec = schema(%{})
 
     response_spec = {:delegate, &SquareUp.ResponseSchema.batch_change_inventory_response/0}
